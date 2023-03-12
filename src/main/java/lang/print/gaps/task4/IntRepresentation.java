413420ff88d07237c9b51edd2cf15e293dadbc78
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-12 18:33:14 +0300

    Update IntRepresentation.java

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,15 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+//        for(int i =0; i < 128 ;i++){
+//            char simbol =  (char)i;
+//            System.out.println(i + " - " + simbol);
+//        }
+        //Print chars :'c', 'h', 'a', 'r' using int 
+        short[] array = new short[]{99, 104, 97, 114};
+        for(int i = 0; i < array.length ;i++){
+            char simbol =  (char)array[i];
+            System.out.println(i + " - " + simbol);
+        }
     }
 }
