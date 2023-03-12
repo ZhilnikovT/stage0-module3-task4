a729616d5dcfbaad00dbeeb0656df78bb70b784e
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-12 18:41:09 +0300

    Update FormulaCreator.java

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a = 1, b = 1, c;
+        c = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
